50317d09953fc136770b9e388c96b35b8ed17f08
wangdana <wangdana1998@gmail.com>
2021-05-14 20:32:18 +0800

    制作表格

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { TableDemo } from "./Table";
+import { Layout } from "antd";
+import Title from "antd/lib/typography/Title";
 
-function App() {
+
+
+const { Content } = Layout;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout>
+        <Content>
+          <Title >React + Ant Design 制作表格</Title>
+          <TableDemo />
+        </Content>
+      </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/components/EditableCell.tsx
@@ -0,0 +1,55 @@
+import { Form } from 'antd';
+import {Input, InputNumber} from 'antd';
+import React from 'react';
+
+interface Item {
+    key: string;
+}
+
+
+interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
+    editing: boolean;
+    dataIndex: string;
+    title: any;
+    inputType: 'number' | 'text';
+    record: Item;
+    index: number;
+    children: React.ReactNode;
+}
+
+
+const EditableCell: React.FC<EditableCellProps> = ({
+                                                       editing,
+                                                       dataIndex,
+                                                       title,
+                                                       inputType,
+                                                       record,
+                                                       index,
+                                                       children,
+                                                       ...restProps
+                                                   }) => {
+    const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+
+    return (
+        <td {...restProps}>
+            {editing ? (
+                <Form.Item
+                    name={dataIndex}
+                    style={{ margin: 0 }}
+                    rules={[
+                        {
+                            required: true,
+                            message: `Please Input ${title}!`,
+                        },
+                    ]}
+                >
+                    {inputNode}
+                </Form.Item>
+            ) : (
+                children
+            )}
+        </td>
+    );
+};
+
+export {EditableCell}
